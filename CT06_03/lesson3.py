4891aeac76fe86e4b55fe8fb6979bdf646201084
Computhink-CS146 <cs146@computhink.com.sg>
2026-03-08 03:05:40 +0000

    end of lesson 7

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,46 @@
 print("Hello from lesson 3")
+
+
+# variable1 = 3
+# variable2 = 5
+# print( variable1 + variable2)
+# print( variable2 - variable1)
+# print( variable1 * variable2)# ** means 3 to the power of 5
+# print( variable1 / variable2)
+
+# print(round(45.3455,3))
+# print(round(456.7)-2)
+
+# print(round(42.56789,2))
+# print(round(1267.15))
+
+# a = 3
+# b = 4
+# print(a + b)
+
+# print((50 - 10)* 3)
+
+# print(2*15)
+# print(58)
+
+# print(96+18*3+2*6)
+# print (10/100 * 162)
+# print(16.2 + 162)
+# print(178.2 - 25)
+
+
+# money = 0
+# money = 100 * 5
+# print (money)
+# money = 20 * 8 + money
+# print (money)
+# money = 20 * 2.50 + money
+# print(money)
+# discount = 10/100 * money
+# print (discount)
+# money = money + discount
+# print (money)
+# gst = 9/100 * money
+# print (gst)
+# money = money + gst
+# print("$",round(money,1))
--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,21 @@
-print("Hello from lesson 4")
+# print("Hello from lesson 4")
+# # myname =  input("what is your name?")
+# # print("hello " + myname)
+# # wall = input("what is your fav color?")
+# # print("i also like " + wall)
+# # mouse = input("what is your age?")
+# # print("i aslo am " + mouse)
+# banana = "Jackie Chan"
+# apple = "I am hangry D:<"
+# print(banana + "says" + apple + ". I will like to eat bean sauce.")
+
+# hobby = input("what do you do in yor free time?")
+# print ("I love " + hobby + "too.")
+
+# things = 1
+# woahhh = 17.95
+# print ("please pay me $", round(things * woahhh, 2))
+
+number = input("choose a random number")
+oh = int(number) * 2
+print("Double your number is " +  str(oh))
